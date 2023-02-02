261ae86198cc7decacbe092543dc972beb8e5752
Authenticode <developers.authenticode@gmail.com>
2023-02-02 15:07:14 +0530

    Committed by akash on 02-02-2023

--- app/src/main/java/com/playMatch/ui/matches/adapter/upcomingAdapter/UpcomingAdapter.kt
+++ app/src/main/java/com/playMatch/ui/matches/adapter/upcomingAdapter/UpcomingAdapter.kt
@@ -7,18 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.playMatch.controller.utils.CommonUtils
-import com.playMatch.databinding.RvChildHomeListItemBinding
 import com.playMatch.databinding.RvMatchesListItemBinding
 import com.playMatch.ui.home.model.HomeChildModel
 import com.playMatch.ui.inbox.activity.chatActivity.ChatActivity
 import com.playMatch.ui.matches.activity.matchDetails.MatchDetailsActivity
-import com.saetae.controller.sharedPrefrence.PrefData
 
 class UpcomingAdapter(var list: ArrayList<HomeChildModel>, var activity: Activity) : RecyclerView.Adapter<UpcomingAdapter.ViewHolder>() {
 
-
-    private val USER = 0
-    private val IMAGE = 1
 
     private  var selectedPosition=-1
     inner class ViewHolder(val binding: RvMatchesListItemBinding) : RecyclerView.ViewHolder(binding.root)
@@ -35,18 +30,17 @@ class UpcomingAdapter(var list: ArrayList<HomeChildModel>, var activity: Activit
     }
 
 
-    @SuppressLint("ResourceAsColor", "SuspiciousIndentation")
+    @SuppressLint("ResourceAsColor", "SuspiciousIndentation", "SetTextI18n", "NotifyDataSetChanged")
     override fun onBindViewHolder(holder: ViewHolder, @SuppressLint("RecyclerView") position: Int) {
         holder.apply {
-            val ItemsviewModel = list[position]
-            val id= PrefData.getStringPrefs(activity, PrefData.CHECK_BOX,"")
+            val itemsViewModel = list[position]
 
 //            if (id=="1"){
 //                binding.cardView.setCardBackgroundColor(Color.parseColor("#F95047"))
 //            }else{
 //                binding.cardView.setCardBackgroundColor(Color.parseColor("#80F95047"))
 //            }
-            holder.binding.name.text = ItemsviewModel.name
+            holder.binding.name.text = itemsViewModel.name
             if (position==1){
                 binding.genderTv.text="Female"
                 binding.host.visibility=View.GONE
